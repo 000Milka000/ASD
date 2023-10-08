329dabe2921d427fbc416241818a06ebae4439f1
000Milka000 <vicktoriaposysaeva@yandex.ru>
2023-10-08 21:36:56 +0300

    Фиксация

--- asd.js
+++ asd.js
@@ -1,32 +1,43 @@
-class Animal {
-    constructor (an, color){
-        this.an = an,
-        this.color = color
-    }
+// class Animal {
+//     constructor (an, color){
+//         this.an = an,
+//         this.color = color
+//     }
 
-    sss(){
-        console.log(`Это ${this.an}. Она ${this.color} цвета. `)
-    }
-}
+//     sss(){
+//         console.log(`Это ${this.an}. Она ${this.color} цвета. `)
+//     }
+// }
 
-let cat = new Animal("Кошка", "чёрного");
-cat.sss();
+// let cat = new Animal("Кошка", "чёрного");
+// cat.sss();
 
-let dog = new Animal("собака", "рыжего");
-dog.sss();
+// let dog = new Animal("собака", "рыжего");
+// dog.sss();
 
-let wolf = new Animal("волчиха", "серого");
-wolf.sss();
+// let wolf = new Animal("волчиха", "серого");
+// wolf.sss();
   
 
-console.log("")
+// console.log("")
 
-let fruit = {
-    banan: 5,
-    apple: 2,
-    orange: 14
-};
+// let fruit = {
+//     banan: 5,
+//     apple: 2,
+//     orange: 14
+// };
 
-for (let k in fruit){
-    console.log(`Это фрукт ${k}.`);
-}
+// for (let k in fruit){
+//     console.log(`Это фрукт ${k}.`);
+// }
+
+
+
+
+let fruit = ["apple", "orange", "banana"];
+
+fruit.forEach((q,w,e) => {
+    console.log(`Это фрукт ${q}.`)
+    console.log(`Его номер ${w+1}`)
+    console.log(`Исходный массив ${e}`)
+    console.log("")})
